100f9bb0f8f896647474ec937e3cc7c3ce83af31
Asavei16 <asaveiflo@gmail.com>
2023-12-15 14:57:19 +0200

    fix bug

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+LONG_BREAK_MIN =  20
+reps = 0
 timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
