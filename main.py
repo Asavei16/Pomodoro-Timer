16469eb7eb40fd6a67b6e61f8e71da790c02bfa0
Asavei16 <asaveiflo@gmail.com>
2023-12-15 14:53:33 +0200

    timer reset

--- main.py
+++ main.py
@@ -9,8 +9,16 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer")
+    check_mark.config(text="")
+    global reps
+    reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
@@ -42,7 +50,8 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count -1)
+         global timer
+         timer = window.after(1000, count_down, count -1)
     else:
         start_timer()
         marks = ""
@@ -73,7 +82,7 @@ canvas.grid(column=1, row=1)
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-start_button = Button(text="Reset", highlightthickness=0)
+start_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 start_button.grid(column=2, row=2)
 
 check_mark = Label(fg=GREEN, bg=YELLOW)
